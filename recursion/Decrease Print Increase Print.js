cdab8afc679a60de6b588c0f30c136d93c9bbb74
saif520 <saifuddin2k17@gmail.com>
2023-01-07 00:40:01 +0530

    Decrease Print Increase Print

---
+++ recursion/Decrease Print Increase Print.js
@@ -0,0 +1,28 @@
+function printDI(n){
+	if(n==0){
+		return;
+	}
+	console.log(n);
+	printDI(n-1);
+	console.log(n);
+	
+}
+
+function runProgram(input){
+	let n=+input;
+	printDI(n);
+}
+
+
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let read="";
+process.stdin.on("data",function(input){
+    read+=input;
+});
+process.stdin.on("end",function(){
+    read=read.replace(/\$n/,"");
+    read=read.replace(/\$n/,"");
+    runProgram(read);
+});
